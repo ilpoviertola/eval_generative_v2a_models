9ad8629b7f409befbd5d1f075d34150d7a55d6a3
Ilpo Viertola <ilpo.viertola@tuni.fi>
2024-05-20 17:54:20 +0300

    fix(fad): file handling fix

--- metrics/fad.py
+++ metrics/fad.py
@@ -1,6 +1,24 @@
 from typing import Optional
+from pathlib import Path
 
 from frechet_audio_distance import FrechetAudioDistance
+
+
+def create_fad_audio_dir(fad_audio_dir: str) -> str:
+    audios_path = Path(fad_audio_dir) / "fad_audios"
+    audios_path.mkdir(exist_ok=False, parents=False)
+    # create symbolic links to the files
+    for audio in Path(fad_audio_dir).glob("*.wav"):
+        (audios_path / audio.name).symlink_to(audio)
+    return audios_path.as_posix()
+
+
+def remove_fad_audio_dir(fad_audio_dir: str) -> None:
+    # remove contents
+    for audio in Path(fad_audio_dir).glob("*.wav"):
+        audio.unlink()
+    # remove empty dir
+    Path(fad_audio_dir).rmdir()
 
 
 def calculate_fad(
@@ -17,6 +35,12 @@ def calculate_fad(
     dtype: str = "float32",
 ) -> float:
     """Calculate the Frechet Audio Distance."""
+    # Copy audio files (if provided) to own directories since FAD just reads the whole dir
+    if gt_audios is not None:
+        gt_audios = create_fad_audio_dir(gt_audios)
+    if sample_audios is not None:
+        sample_audios = create_fad_audio_dir(sample_audios)
+
     fad = FrechetAudioDistance(
         model_name=model_name,
         sample_rate=sample_rate,
@@ -32,6 +56,11 @@ def calculate_fad(
         eval_embds_path=gt_embds_path + "/" + embeddings_fn,
         dtype=dtype,
     )
+
+    if gt_audios is not None:
+        remove_fad_audio_dir(gt_audios)
+    if sample_audios is not None:
+        remove_fad_audio_dir(sample_audios)
 
     if verbose:
         print("FAD:", score)
